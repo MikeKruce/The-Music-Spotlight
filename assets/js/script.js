a9c38f6acc3e6d531a6582e48f3db7428db9a12b
codecj93 <159395918+codecj93@users.noreply.github.com>
2024-04-11 20:47:02 -0400

    API code added

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+//spotify artists
+// const url = 'https://spotify23.p.rapidapi.com/artist_overview/?id=2w9zwq3AktTeYYMuhMjju8';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '33c405c45fmsh66b0459cec78964p1cdabdjsn132baa4ed581',
+// 		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+
+// Billboard
+// const url = 'https://billboard-api2.p.rapidapi.com/artist-100?date=2019-05-11&range=1-10';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '33c405c45fmsh66b0459cec78964p1cdabdjsn132baa4ed581',
+// 		'X-RapidAPI-Host': 'billboard-api2.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
