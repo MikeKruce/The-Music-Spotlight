5229de3745d9c90e913925bd8645907475cdaa1b
codecj93 <159395918+codecj93@users.noreply.github.com>
2024-04-16 18:26:43 -0400

    Update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Billboard
+// const url = 'https://billboard-api2.p.rapidapi.com/artist-100?date=2019-05-11&range=1-10';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '33c405c45fmsh66b0459cec78964p1cdabdjsn132baa4ed581',
+// 		'X-RapidAPI-Host': 'billboard-api2.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
